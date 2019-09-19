ec7b82d7d1e2f7ef3bfc627afb1e4c469e354552
rajusingh41 <raju.singh41@gmail.com>
2019-09-19 14:06:31 +0530

    Add project files.

---
+++ JsonToDataTable/Program.cs
@@ -0,0 +1,115 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace JsonToDataTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var filePath = @"D:\LocalIIS2019\JsonToDataTable\JsonToDataTable\MyJson2.json";
+
+
+            var jsonData = File.ReadAllText(filePath);
+
+            var dictionary = JsonConvert.DeserializeObject<Dictionary<string, object>>(jsonData);
+
+            foreach (KeyValuePair<string, object> item in dictionary)
+            {
+                Console.WriteLine("Key: {0}, Value: {1}", item.Key, item.Value);
+            }
+
+
+            var dt = JsonStringToDataTable(jsonData);
+        }
+
+       
+
+        public static DataTable JsonStringToDataTable(string jsonString)
+        {
+            DataTable dt = new DataTable();
+            string[] jsonStringArray = Regex.Split(jsonString.Replace("[", "").Replace("]", ""), "},{");
+            List<string> ColumnsName = new List<string>();
+            foreach (string jSA in jsonStringArray)
+            {
+                string[] jsonStringData = Regex.Split(jSA.Replace("{", "").Replace("}", ""), ",");
+                foreach (string ColumnsNameData in jsonStringData)
+                {
+                    try
+                    {
+                        int idx = ColumnsNameData.IndexOf(":");
+                        string ColumnsNameString = ColumnsNameData.Substring(0, idx - 1).Replace("\"", "");
+                        if (!ColumnsName.Contains(ColumnsNameString))
+                        {
+                            ColumnsName.Add(ColumnsNameString);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        throw new Exception(string.Format("Error Parsing Column Name : {0}", ColumnsNameData));
+                    }
+                }
+                break;
+            }
+            foreach (string AddColumnName in ColumnsName)
+            {
+                dt.Columns.Add(AddColumnName);
+            }
+            foreach (string jSA in jsonStringArray)
+            {
+                string[] RowData = Regex.Split(jSA.Replace("{", "").Replace("}", ""), ",");
+                DataRow nr = dt.NewRow();
+                foreach (string rowData in RowData)
+                {
+                    try
+                    {
+                        int idx = rowData.IndexOf(":");
+                        string RowColumns = rowData.Substring(0, idx - 1).Replace("\"", "");
+                        string RowDataString = rowData.Substring(idx + 1).Replace("\"", "");
+                        nr[RowColumns] = RowDataString;
+                    }
+                    catch (Exception ex)
+                    {
+                        continue;
+                    }
+                }
+                dt.Rows.Add(nr);
+            }
+            return dt;
+        }
+
+        //public static DataTable Tabulate(string json)
+        //{
+        //    var jsonLinq = JObject.Parse(json);
+
+        //    // Find the first array using Linq
+        //      var srcArray = jsonLinq.Descendants().Where(d => d is JArray).First();
+        // //   var srcArray = jsonLinq.Descendants();
+        //    var trgArray = new JArray();
+        //    foreach (JObject row in srcArray.Children<JObject>())
+        //    {
+        //        var cleanRow = new JObject();
+        //        foreach (JProperty column in row.Properties())
+        //        {
+        //            // Only include JValue types
+        //            if (column.Value is JValue)
+        //            {
+        //                cleanRow.Add(column.Name, column.Value);
+        //            }
+        //        }
+
+        //        trgArray.Add(cleanRow);
+        //    }
+
+        //    return JsonConvert.DeserializeObject<DataTable>(trgArray.ToString());
+        //}
+    }
+}
